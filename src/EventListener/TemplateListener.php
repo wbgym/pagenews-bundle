19c57811f59ed1fb9e1781e1b411c925d07b4a4b
Johannes <johannes@jonesmedia.de>
2018-09-16 22:12:53 +0200

    Add more template vars

--- src/EventListener/TemplateListener.php
+++ src/EventListener/TemplateListener.php
@@ -24,6 +24,10 @@ class TemplateListener
      */
     public function onParseTemplate($template)
     {
+        /**
+         * On newslist Template with standard tag filter:
+         * Add Title and alias of first standard tag to template
+         */
         if ($template->news_filterDefault != null)
         {
             $tagId = unserialize($template->news_filterDefault)[0];
@@ -31,6 +35,39 @@ class TemplateListener
             $objModel = NewsCategoryModel::findById($tagId);
             $template->tagTitle = $objModel->getTitle();
             $template->tagAlias = $objModel->alias;
+        }
+
+        /**
+         * On categories navigation template:
+         * Add currently active filter item to 1st-level template
+         */
+        if 
+        (
+            $template->type == 'Codefog\NewsCategoriesBundle\FrontendModule\NewsCategoriesModule'
+            && $template->level == 'level_1'
+        )
+        {
+            // Get alias of currently active item via URL parameter
+            global $objPage;
+            $objRootPage = \PageModel::findById($objPage->rootId);
+            $tagUrlParameter = $objRootPage->newsCategories_param;
+            
+            if ($tagUrlParameter)
+            {
+                $filter = \Input::get($tagUrlParameter);
+            }
+            else 
+            {
+                $filter = \Input::get('category');  // default URL param
+            }
+            
+            // Get the object by alias and send it to template
+            if ($filter)
+            {
+                $objActive = NewsCategoryModel::findBy('alias', $filter);
+                $template->activeItem = $objActive;
+                dump($objActive);
+            }
         }
     }
 
@@ -57,7 +94,8 @@ class TemplateListener
         
         // Generate cutTeaser
         $objTemplate->cutTeaser = Teaser::generateCutTeaser($objTemplate->teaser);
+
+        // Send category filter page to template
+        $objTemplate->tagFilterPage = $objModule->news_categoryFilterPage;
     }
-
-
 }
